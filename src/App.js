d4c20887054cb19565f547206b6f60411964dab7
sostene <ngarukiyimanasostene@gmail.com>
2022-02-12 14:34:23 +0200

    second change of codes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    console.log(data.hits)
   } 
 
   // state of searching
